f9310366fcb4bcfd3cc01ad2d75e97bb37d59a18
Shamima Akther <65592303+1834902616@users.noreply.github.com>
2020-05-19 23:27:30 +0600

    Add files via upload

---
+++ lab1/7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int array[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, j, size = 10;
+    for( i = 0; i<size ; i++)
+    {
+        printf ("%d ",array[i]);
+    }
+    printf("\n");
+
+    for( i = size - 1 ; i >= 0 ; i--)
+    {
+        printf ("%d ",array[i]);
+    }
+    return 0;
+}
+
+
