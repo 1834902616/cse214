abfaa8ee5b6a42329e365c7411e1781381416836
Shamima Akther <65592303+1834902616@users.noreply.github.com>
2020-05-20 12:01:54 +0600

    Add files via upload

---
+++ lab1/6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    int array[100]= {1,2,3,4,5,6,7,8,9,10};
+    int i, size = 10;
+    for( i = 0; i<size ; i++)
+    {
+        printf("%d",array[i]);
+
+        if( i <= size - 2)
+        {
+            printf(",");
+        }
+    }
+    return 0;
+}
+
+
