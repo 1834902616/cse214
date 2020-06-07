0f045ae84f447274f07e354a59168b822edd2070
Shamima Akther <65592303+1834902616@users.noreply.github.com>
2020-06-07 23:46:52 +0600

    Update 6.c

--- lab4/6.c
+++ lab4/6.c
@@ -1,32 +1,40 @@
-#include<stdio.h>
-int main ()
-{
-    int array[10] = {1,2,3,4,5};
-    int arr[10] = {6,7,8,9,10};
-    int a[10], i, j, size_a, size_array = 5, size_arr = 5;
-    size_a = size_array + size_arr;
+/*Merge the Elements of two sorted array*/
 
+#include<stdio.h>
+int main()
+{
+    int a[5] = {8,13,45,6,9}, size_a = 5;
+    int arr[5] = {5,4,3,16,20}, i, j, size_arr = 5;
+    int array[10], temp = 0;
+    int size_array = size_a + size_arr;
+    for(i = 0; i < size_a; i++)
+    {
+        array[i] = a[i];
+    }
+    for(i = 0, j = size_a; i < size_array; i++,j++)
+    {
+        array[j] = arr[i];
+    }
     for(i = 0; i < size_array; i++)
     {
         printf("%d ",array[i]);
     }
     printf("\n");
-    for(i = 0; i < size_arr; i++)
-    {
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
     for(i = 0; i < size_array; i++)
     {
-        a[i] = array[i];
+        for(j = i + 1; j < size_array; j++)
+        {
+            if(array[i] > array[j])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
     }
-    for(i = 0, j = size_array; i < size_arr; i++,j++)
+    for(i = 0; i < size_array; i++)
     {
-        a[j] = arr[i];
-    }
-    for(i = 0; i < size_a; i++)
-    {
-        printf("%d ",a[i]);
+        printf("%d ",array[i]);
     }
     return 0;
 }
